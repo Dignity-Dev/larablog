5ef1f0359bc938fd99aab0a0bec74fafab1bcd3b
Basheer <you@example.com>
2022-06-09 20:03:34 +0100

    new blade

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','Blog Homepage')}} </title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Services</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, in? Libero in iure numquam natus, animi ipsa ab mollitia soluta eaque ullam modi? Quo, est placeat nam nesciunt ab et?</p>
+    </body>
+</html>
